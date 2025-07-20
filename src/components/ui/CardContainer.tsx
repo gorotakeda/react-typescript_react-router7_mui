905f3db213e49612afae95b4fcdbe0857988d137
goro takeda <rugbytkg3@gmail.com>
2025-07-20 13:50:53 +0900

    インデント修正

--- src/components/ui/CardContainer.tsx
+++ src/components/ui/CardContainer.tsx
@@ -21,32 +21,32 @@ interface CardContainerProps {
 export const CardContainer = ({ title, subTitle, content }: CardContainerProps) => {
     return (
         <Container maxWidth="md">
-                <Box sx={{ mt: 4, mb: 2 }}>
-                    <Typography variant="h3" component="h1" gutterBottom color="primary">
-                        {title}
-                    </Typography>
-                    <Card elevation={3}>
-                        <CardActionArea>
-                            <CardContent>
-                                <Typography variant="h6" gutterBottom>
-                                    {subTitle}
-                                </Typography>
-                                <Stack spacing={2}>
-                                    {content.map((content, index) => (
-                                        <Box key={index}>
-                                            <Typography variant="subtitle1" fontWeight="bold">
-                                                {content.contentTitle}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                {content.content}
-                                            </Typography>
-                                        </Box>
-                                    ))}
-                                </Stack>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Box>
+            <Box sx={{ mt: 4, mb: 2 }}>
+                <Typography variant="h3" component="h1" gutterBottom color="primary">
+                    {title}
+                </Typography>
+                <Card>
+                    <CardActionArea>
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                {subTitle}
+                            </Typography>
+                            <Stack spacing={2} direction="row">
+                                {content.map((content, index) => (
+                                    <Box key={index}>
+                                        <Typography variant="subtitle1" fontWeight="bold">
+                                            {content.contentTitle}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {content.content}
+                                        </Typography>
+                                    </Box>
+                                ))}
+                            </Stack>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Box>
         </Container>
     )
 }
